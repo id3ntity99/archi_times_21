66fcf36ceecab59c2e2f2f477e4ccf1ae9e6a240
id3ntity99 <jas06113@gmail.com>
2021-11-04 16:47:24 +0900

    Added functions

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
 const express = require("express");
 const app = express();
 const { data } = require("./backend/test");
+const {
+  getArticles,
+  getDetail,
+} = require("./backend/controllers/news.controller");
 const PORT = 5000;
 
 //Static
 app.use(express.static("./frontend/public"));
+
 app.get("/", (req, res) => {
   res.send("Hello");
 });
+
+// Use imported midlleWares
+app.use(getArticles, getDetail);
 app.get("/news", (req, res) => {
-  // const newArticle = data.map((article) => {
-  //   const { title, id } = article;
-  //   return { title, id };
+  // const newArticles = data.map((article) => {
+  //   const { title, desc } = article;
+  //   return { title, desc };
   // });
-  // res.json(newArticle);
-  console.log(req.query);
-  res.json(data);
+  // res.json(newArticles);
 });
 
 app.listen(PORT, () => {
--- backend/controllers/news.controller.js
+++ backend/controllers/news.controller.js
@@ -1,1 +1,23 @@
 const { data } = require("../test.js");
+
+// MiddleWare
+function getArticles(req, res, next) {
+  if (!req.query.detail) {
+    return res.json(data);
+  }
+  next();
+}
+
+function getDetail(req, res, next) {
+  if (req.query.detail) {
+    const detail = data.filter((article) => article.id === req.query.detail);
+    const newDetail = detail.map((detail) => {
+      const { title, desc, link } = detail;
+      return { title, desc, link };
+    });
+    return res.json(newDetail);
+  }
+  next();
+}
+
+module.exports = { getArticles, getDetail };
